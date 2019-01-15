75f06c8b214fac309dc1784bc6057560433e0a0c
RWGaMeR <puxxo9700@gmail.com>
2019-01-15 10:29:38 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -27,3 +27,9 @@ if(strpos($text, "/start") === 0)
 {
 	$response = "$time" ;
 }
+
+header("Content-Type: application/json");
+$parameters = array('chat_id' => $chatId, "text" => $response);
+$parameters["method"] = "sendMessage";
+echo json_encode($parameters);
+?>
